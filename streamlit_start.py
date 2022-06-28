5ad91c8e4432f01f5c55f8e820af176296cafd31
Eugen Kirichenko <e.kirichenko@gmail.com>
2022-06-28 16:49:27 +0300

    Update streamlit_start.py

--- streamlit_start.py
+++ streamlit_start.py
@@ -3,19 +3,19 @@ import io
 import streamlit as st
 from PIL import Image
 import numpy as np
-from tensorflow.keras.applications import EfficientNetB7
+from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
 def load_model():
-    model = EfficientNetB7(weights='imagenet')
+    model = EfficientNetB0(weights='imagenet')
     return model
 
 
 
 def preprocess_image(img):
-    img = img.resize((256, 256))
+    img = img.resize((224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
